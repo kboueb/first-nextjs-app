86686f9c4ef6c8477e69f57204585c3ceb5a5ae3
kboueb <kani.bouebassihou@insign.africa>
2023-10-10 08:42:01 +0000

    Initialisation2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import styles from "./page.module.css"
 
 const About = () => {
     return (
-        <div>About</div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}></div>
+            <div className={styles.textContainer}></div>
+        </div>
     )
 }
 
